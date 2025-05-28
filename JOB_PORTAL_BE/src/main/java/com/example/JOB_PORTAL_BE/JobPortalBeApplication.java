51b6fc37f00e222ccba2c63891f0452f18271e0b
raksindha <raksheen9899@gmail.com>
2025-05-28 20:15:27 +0530

    backend setup added

---
+++ JOB_PORTAL_BE/src/main/java/com/example/JOB_PORTAL_BE/JobPortalBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.JOB_PORTAL_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalBeApplication.class, args);
+	}
+
+}
